2c9ad59455057fe0f7a664c531536b8f2a844f23
kyjna0312 <kyjna0312@gmail.com>
2018-01-10 20:25:49 +0900

    findAll 사용

--- scrapingtest.py
+++ scrapingtest.py
@@ -1,21 +1,9 @@
 from urllib.request import urlopen
-from urllib.request import HTTPError
 from bs4 import BeautifulSoup
 
-def getTitle(url):
-    try:
-        html = urlopen(url)
-    except HTTPError as e:
-        return None
-    try:
-        bsObj = BeautifulSoup(html.read(), "html.parser")
-        title = bsObj.body.h1
-    except AttributeError as e:
-        return None
-    return title
-
-title = getTitle("http://www.pythonscraping.com/pages/page1.html")
-if title == None:
-    print("title could not be found")
-else:
-    print(title)
+html = urlopen("http://www.pythonscraping.com/pages/warandpeace.html")
+bsObj = BeautifulSoup(html, "html.parser")
+nameList = bsObj.findAll({"h1", "h2", "h3", "h4", "h5", "h6"})
+print(nameList)
+for name in nameList:
+    print(name.get_text())
