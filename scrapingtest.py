53a821837f363de29a5ce55d8068b315471ec8a5
kyjna0312 <kyjna0312@gmail.com>
2018-01-10 19:50:04 +0900

    예외처리 과정을 함수로 작성

--- scrapingtest.py
+++ scrapingtest.py
@@ -2,9 +2,20 @@ from urllib.request import urlopen
 from urllib.request import HTTPError
 from bs4 import BeautifulSoup
 
-try:
-    html = urlopen("http://pythonscraping.com/pages/page1.html")
-except HTTPError as e:
-    print(e)
+def getTitle(url):
+    try:
+        html = urlopen(url)
+    except HTTPError as e:
+        return None
+    try:
+        bsObj = BeautifulSoup(html.read(), "html.parser")
+        title = bsObj.body.h1
+    except AttributeError as e:
+        return None
+    return title
+
+title = getTitle("http://www.pythonscraping.com/pages/page1.html")
+if title == None:
+    print("title could not be found")
 else:
-    print(bsObj.h1)
+    print(title)
