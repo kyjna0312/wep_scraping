9202157947dd57835a3767b078510415ddd3424d
kyjna0312 <kyjna0312@gmail.com>
2018-01-12 21:34:24 +0900

    span을 기초로 한 크롤링

--- naver_scraping.py
+++ naver_scraping.py
@@ -1,3 +1,13 @@
 # 2013250012 김용진
 # 2018 - 01 - 12
 # 해당 요일에 해당하는 네이버 인기검색어를 크롤링하는 python 프로그램
+
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+html = urlopen("https://datalab.naver.com/keyword/realtimeList.naver?where=main")
+bsObj = BeautifulSoup(html, "html.parser")
+
+span = bsObj.findAll("span", {"class" : "title"})
+for text in span:
+    print(text.get_text())
