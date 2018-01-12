b3560580153b2d3d34623f256f08d66d1a28f718
kyjna0312 <kyjna0312@gmail.com>
2018-01-12 19:39:36 +0900

    데이터 수집

--- scrapingtest.py
+++ scrapingtest.py
@@ -1,21 +1,25 @@
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
-import datetime
-import random
 import re
 
-random.seed(datetime.datetime.now())
+pages = set()
 
-def getLinks(articleUrl):
-    html = urlopen("https://en.wikipedia.org/"+articleUrl)
+def getLinks(pageUrl):
+    global pages
+    html = urlopen("https://en.wikipedia.org/"+pageUrl)
     bsObj = BeautifulSoup(html, "html.parser")
+    try:
+        print(bsObj.h1.get_text())
+        print(bsObj.find(id="mw-bodyContent-text").findAll("p")[0])
+        print(bsObj.find(id="ca-edit").find("span").find("a").attrs['href'])
+    except AttributeError:
+        print("This page is missing somthing! No worries thought!")
+    for link in bsObj.findAll("a", href=re.compile("^(/wiki/)")):
+        if 'href' in link.attrs:
+            if link.attrs['href'] not in pages:
+                newPage = link.attrs['href']
+                print("---------------\n"+newPage)
+                pages.add(newPage)
+                getLinks(newPage)
 
-    return bsObj.find("div", {"id":"bodyContent"}).findAll("a", href=re.compile("^(/wiki/)((?!:).)*$"))
-
-links = getLinks("/wiki/Kevin_Bacon")
-print(type(links))
-
-while len(links) > 0:
-    newArticle = links[random.randint(0, len(links)-1)].attrs["href"]
-    print(newArticle)
-    links = getLinks(newArticle)
+getLinks("")
